56a6efa016217e089df7dac36cb4ce10315d5042
Flushimoto <njoni.prishtine@gmail.com>
2025-08-27 19:42:05 +0200

    Update layout with body class and metadata description

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,17 +4,16 @@ import JupiterProvider from "./providers/JupiterProvider";
 
 export const metadata: Metadata = {
   title: "Macho Coin ($MACHO)",
-  description: "The official MACHO website"
+  description: "The official MACHO website",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="!scroll-smooth">
       <head>
-        {/* Jupiter modal runtime (modal version) */}
         <script src="https://terminal.jup.ag/main-v2.js"></script>
       </head>
-      <body>
+      <body className="font-sans">
         <JupiterProvider />
         {children}
       </body>
